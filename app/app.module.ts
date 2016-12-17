11c683f199a44a67c6ef5443476b93dd6efa9451
Emmancipate Musemwa <emanzoelife@gmail.com>
2016-12-17 22:16:55 +0800

    Added Pipe for listing categories

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,12 @@
 import  { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  } from '@angular/forms';
 
 import  { AppComponent } from './app.component';
 import  { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
 
 @NgModule({
 	imports: [
@@ -14,7 +16,8 @@ import { FavoriteDirective } from './favorite.directive';
 		AppComponent,
 		MediaItemComponent,
 		MediaItemListComponent,
-		FavoriteDirective
+		FavoriteDirective,
+		CategoryListPipe
 	],
 	bootstrap:[
 		AppComponent
